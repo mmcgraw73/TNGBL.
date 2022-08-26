5ddb74803c9cee3a48502b7ff8755f566984dccb
mmcgraw73 <shout@mcgraw.io>
2022-08-25 22:25:52 -0400

    repeater

--- repeater.class.js
+++ repeater.class.js
@@ -1,21 +1,17 @@
-var torepeat=document.querySelectorAll("[data-repeat]");
-
-for (a=0;a<torepeat.length;a+=1) {
-    var el=torepeat[a];
-    var rpt=el.getAttribute("data-repeat");
-
-    for (r=0;r<rpt;r+=1) {
-        el.insertAdjacentHTML('afterend',el.cloneNode(true).outerHTML);
-    }
-}
-
-
 export const Repeater = class {
-  constructor(name, htmlStr, el) {
-    this.name = name;
-    this.htmlStr = htmlStr;
-    this.el = el
+  constructor(selectorString) {
+    this.selectorString = selectorString;
   }
 
-  // element 
-}
+  $ = {
+    selector: this.selectorString,
+    el: document.querySelector(selector),
+  };
+
+  hasEl = () => this.$.el.children.length;
+
+  htmlIn = (pos) =>
+    this.hasEl
+      ? $.el.insertAdjacentHTML(pos, el.cloneNode(true).outerHTML)
+      : '';
+};
